34bef983b83c6ea8fe20ca116735cd978c37cfff
CONVELBO <convelboismaila@gmail.com>
2024-04-23 20:42:10 +0000

    Ajout des fichiers de base pour la gestion des emplois du temps

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('welcomeButton').addEventListener('click', function() {
+    document.getElementById('welcomeMessage').textContent = 'Nous sommes ravis de vous accueillir sur note site. Explorez nos programmes et nos initiatives!';
+});
