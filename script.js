e9338d026e7d0b3c63e72c6bd82d83fa873499c5
CONVELBO <convelboismaila@gmail.com>
2024-04-25 15:08:47 +0000

    Ajout d'une galerie d'images pour la gestion des emplois du temps

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function changeDynamicContent() {
 window.onload = function() {
     changeDynamicContent();
 };
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll("#galerie img");
+    images.forEach(image => {
+    image.addEventListener("click", () => {
+    alert("Vous avez cliqué sur une image !");
+    });
+    });
+    });
